2b244b8eb08a3b67081ec45284c724235056ae4a
PinedaAgudelo2003 <brahianpineda12@gmail.com>
2026-04-13 18:16:05 -0500

    Modificacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
 import './styles/global.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
